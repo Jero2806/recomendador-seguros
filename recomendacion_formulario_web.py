ce045885b794d2c0c31aaee13d4a1979bee8feb4
Jero2806 <jeroruedagiraldo@gmail.com>
2025-05-03 20:15:40 -0500

    Correcciones en formulario y lógica Streamlit

--- recomendacion_formulario_web.py
+++ recomendacion_formulario_web.py
@@ -4,16 +4,21 @@ import pandas as pd
 import joblib
 import os
 
+# 🔹 Esto DEBE ir primero
+st.set_page_config(page_title="Recomendador de Seguros", layout="centered")
+
 # Cargar modelo y encoder
 modelo = joblib.load("modelo_regresion_logistica.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
 
-col1, col2, col3 = st.columns([1, 2, 1])
+# 🔹 Mostrar los logos en la parte superior
+col1, col2 = st.columns([1, 2, 1])
 with col1:
-    st.image("static/logo_global.png", use_column_width=True)
+    st.image("static/logo_global.png", use_container_width=True)
 with col2:
-    st.image("static/recomendacion.png", use_column_width=True)
+    st.image("static/recomendacion.png", use_container_width=True)
 
+    
 # Configurar estilo y página
 st.set_page_config(page_title="Recomendador de Seguros", layout="centered")
 st.markdown("""
